ad7b61bd95f33d4ebdbf81006bd0bfb5ecff0182
nayouta47 <nayouta47@naver.com>
2026-04-08 02:58:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/VegiAlter/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        editor: resolve(__dirname, "editor.html"),
+      },
+    },
+  },
 });
